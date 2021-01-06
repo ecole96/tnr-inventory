9aa9b6929abc4ead81b463e468d4c5f2da9fd058
ecole96 <ecole96@outlook.com>
2021-01-06 00:35:20 -0500

    documentation

--- inventory/static/inventory/new_jobpart.js
+++ inventory/static/inventory/new_jobpart.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
-    $('select[name=quantity]').empty();
-
-    $('select[name=part]').change(function(){
-        $('select[name=quantity]').empty();
+    $('select[name=quantity]').empty(); // quantity selection is intially filled with placeholder values, so empty out upon load (to be populated with true values upon selecting part)
+    $('select[name=part]').change(function(){ // occurs when selecting a new part
+        // empty out any current quantity / price values
+        $('select[name=quantity]').empty(); 
         $('input[name=unit_price]').val('');
         part_id = $(this).val();
         if(part_id) {
-        request_url = "/parts/get_data/" + part_id;
+        request_url = "/parts/get_data/" + part_id; // get selected part data with ajax call
         $.ajax({
             url: request_url,
             success: function(data){
                 json = JSON.parse(data);
+                // populate fields with selected part's data
                 $('input[name=unit_price]').val(json.unit_price);
                 for (i = 1; i <= json.quantity; i++) {
                     $('select[name=quantity]').append(
