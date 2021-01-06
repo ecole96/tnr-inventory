fc4864b71a78f1279e117defb6fffc3290d06e6d
ecole96 <ecole96@outlook.com>
2021-01-06 00:31:20 -0500

    add tax toggling (and documentation)
    ;

--- inventory/models.py
+++ inventory/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import RegexValidator, MinValueValidator
 from decimal import Decimal
 
-# Create your models here.
+# represents parts in the inventory (can be added to jobs as a JobPart)
 class Part(models.Model):
     name = models.CharField(max_length=250,help_text = "250 characters max.")
     quantity = models.PositiveIntegerField(help_text="This is the number of parts currently in stock (but not allocated to any jobs).")
@@ -12,6 +12,7 @@ class Part(models.Model):
     def __str__(self):
         return self.name
 
+# self-explanatory - represents repair jobs. Customer data is rolled in with individual Jobs, as normalizing with a Customers table would be overkill for current purposes
 class Job(models.Model):
     STATUS_CHOICES = [
         ("NS","Not Started"),
@@ -23,21 +24,25 @@ class Job(models.Model):
     customer_phone = models.CharField(validators=[phone_regex],max_length=10,blank=True,help_text="No dashes, parentheses, or country code.",verbose_name="Customer Phone")
     customer_email = models.EmailField(blank=True,verbose_name="Customer Email")
     vehicle = models.CharField(max_length=250,blank=True,help_text="What's being repaired?")
-    status = models.CharField(max_length=2,choices=STATUS_CHOICES,default="NS")
-    notes = models.CharField(max_length=1000,blank=True,help_text="Enter any repair notes here.")
+    status = models.CharField(max_length=2,choices=STATUS_CHOICES,default="NS") # current state of repair progress
+    notes = models.CharField(max_length=1000,blank=True,help_text="Enter any repair notes here.") # these will appear on the invoice
     created = models.DateTimeField(auto_now_add=True,verbose_name="Date/Time Created")
 
-    def __str__(self):
+    def __str__(self): # String representation is in "Customer - Vehicle" format
         v_str = self.vehicle if self.vehicle else "N/A"
-        return " - ".join([self.customer_name, v_str])
+        return "#%s / %s" % (self.pk, " - ".join([self.customer_name, v_str]))
 
+# represents a Part being attached to a Job (what part, how many, and how much to charge per part)
 class JobPart(models.Model):
     job = models.ForeignKey(Job,on_delete=models.CASCADE)
     part = models.ForeignKey(Part,on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(help_text="How many instances of this part do you want to apply to this job?")
-    unit_price = models.DecimalField(max_digits=11,decimal_places=2,verbose_name="Unit Price ($)",validators=[MinValueValidator(Decimal('0.00'))])
+    unit_price = models.DecimalField(max_digits=11,decimal_places=2,verbose_name="Unit Price ($)",validators=[MinValueValidator(Decimal('0.00'))]) # default is selected part's unit price, but can be overridden via form
+    tax = models.BooleanField(default=True,blank=True,verbose_name="Tax?",help_text="Check this to charge tax for all quantities of this part (for this job).") # can decide whether to charge tax or not
 
+# represents services performed for Jobs (description of service and how much to charge for it)
 class Service(models.Model):
     job = models.ForeignKey(Job,on_delete=models.CASCADE)
     description = models.CharField(max_length=250,verbose_name="Description")
     price = models.DecimalField(max_digits=11,decimal_places=2,verbose_name="Service Price ($)",validators=[MinValueValidator(Decimal('0.00'))])
+    tax = models.BooleanField(default=False,blank=True,verbose_name="Tax?",help_text="Check this to charge tax on this service.")
--- inventory/utils.py
+++ inventory/utils.py
@@ -2,24 +2,34 @@ from decimal import Decimal
 from .models import Service, JobPart
 from django.db.models import Sum
 
+# used to calculate pricing data for Job Details page and invoices
 def calculate_prices(job,invoice=False):
     prices = {}
-    tax_rate = Decimal('0.06')
+    tax_rate = Decimal('0.06') # Kentucky tax rate
 
-    prices['labor_price'] = Service.objects.filter(job=job).aggregate(Sum('price'))['price__sum'] if Service.objects.filter(job=job).count() > 0 else Decimal('0.00')
-    prices['parts_price'] = sum([Decimal(j.quantity) * j.unit_price for j in JobPart.objects.filter(job=job)]) if JobPart.objects.filter(job=job).count() > 0 else Decimal('0.00')
-    prices['tax'] = round(sum([prices['labor_price'],prices['parts_price']]) * tax_rate,2)
-    if invoice:
+    services = Service.objects.filter(job=job)
+    prices['labor_price'] = services.aggregate(Sum('price'))['price__sum'] or Decimal('0.00') # total labor price
+
+    parts = JobPart.objects.filter(job=job)
+    prices['parts_price'] = sum([j.quantity * j.unit_price for j in parts]) or Decimal('0.00') # total part price
+
+    taxed_services = Service.objects.filter(job=job,tax=True)
+    service_taxes = taxed_services.aggregate(Sum('price'))['price__sum'] or Decimal('0.00') # total of taxed services
+    part_taxes = sum([j.quantity * j.unit_price for j in JobPart.objects.filter(job=job,tax=True)]) or Decimal('0.00') # total of taxed parts
+    prices['tax'] = round(sum([service_taxes,part_taxes]) * tax_rate,2) # calculate final tax
+
+    if invoice: # calculating for invoice - more itemizing necessary
         jobparts = JobPart.objects.filter(job=job)
         services = Service.objects.filter(job=job)
         prices['labor'] = []
         for s in services:
             service_dict = {'desc':s.description, 'price':s.price}
-            prices['labor'].append(service_dict)
+            prices['labor'].append(service_dict) # appending detailed service pricing
         prices['parts'] = []
         for jp in jobparts:
             part_dict = {'name':jp.part.name,'unit_price':jp.unit_price,'quantity':jp.quantity, 'total':jp.unit_price * Decimal(jp.quantity)}
-            prices['parts'].append(part_dict)
+            prices['parts'].append(part_dict) # appending detailed parts pricing
         prices['subtotal'] = prices['labor_price'] + prices['parts_price']
+        
     prices['total'] = sum([prices['labor_price'],prices['parts_price'],prices['tax']])
     return prices
